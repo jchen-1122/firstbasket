bea4a5ae345ccc9f25d6d1475643d3f44c47ac06
unknown <jchen1122@berkeley.edu>
2022-11-06 19:00:04 -0800

    odds and name

--- firstbasket/src/App.js
+++ firstbasket/src/App.js
@@ -7,13 +7,11 @@ function App() {
   const MARKETID = '142'
 
   // states
-  const [events, setEvents] = useState();
-  const [offers, setOffers] = useState();
-  const [participants, setParticipants] = useState();
-  const [selections, setSelections] = useState();
+  const [events, setEvents] = useState([]);
+  const [offers, setOffers] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchEventsData = async () => {
       // fetch events
       const eventsUrl = 'https://api.bettingpros.com/v3/events?sport=NBA&date=2022-11-05'
       const eventsData = await fetch(eventsUrl, {
@@ -24,42 +22,67 @@ function App() {
       const eventsJson = await eventsData.json();
       console.log(eventsJson.events)
       setEvents(eventsJson.events)
-
-      // parse events
-      var eventsString = ''
-
-      for (let i = 0; i < events.length; i++) {
-        eventsString += events[i].id;
-        if (i != events.length - 1) {
-          eventsString += ':'
-        }
-      }
-      
-      // fetch offers
-      const offersUrl = 'https://api.bettingpros.com/v3/offers?sport=NBA&market_id='+MARKETID+'&event_id='+eventsString+'&location=CA&live=true'
-      const offersData = await fetch(offersUrl, {
-        headers: {
-          'X-Api-Key': APIKEY
-        },
-      });
-      const offersJson = await offersData.json();
-      console.log(offersJson.offers)
-      setOffers(offersJson.offers)
     }
   
-    fetchData().catch(console.error);
+    fetchEventsData().catch(console.error);
   }, [])
+
+  const fetchOffersData = async () => {
+    // parse events
+    var eventsString = ''
+
+    for (let i = 0; i < events.length; i++) {
+      eventsString += events[i].id;
+      if (i != events.length - 1) {
+        eventsString += ':'
+      }
+    }
+
+    console.log(eventsString)
+    
+    // fetch offers
+    const offersUrl = 'https://api.bettingpros.com/v3/offers?sport=NBA&market_id='+MARKETID+'&event_id='+eventsString+'&location=CA&live=true'
+    const offersData = await fetch(offersUrl, {
+      headers: {
+        'X-Api-Key': APIKEY
+      },
+    });
+    const offersJson = await offersData.json();
+    console.log(offersJson.offers)
+    setOffers(offersJson.offers)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ul>
-          {
-            events.map(function(event){
-              return <li> {event.id} as the {event.sport} </li>
-            })
-          }
-        </ul> */}
+        <p>Date PlaceHolder</p>
+        <button onClick={fetchOffersData}>
+          Get Odds
+        </button>
+        {events.map((event,eid)=>{
+          return <div key={eid}>
+            <h5>{event.visitor} @ {event.home}</h5>
+            {offers.map((offer,oid) => {
+              if (offer.event_id == event.id) {
+                return <div key={oid}>
+                  {offers.length != 0 ? offer.participants.map((participant,pid)=>{
+                    return <div key={pid}>
+                      <h6>{participant.name} 
+                      {offer.selections.map((selection, sid) => {
+                        if (selection.participant == participant.id) {
+                          return " +" + selection.opening_line.cost
+                        }
+                      })}
+                      </h6>
+                    </div>
+                  }) : <div></div>}
+                </div>
+              } else {
+                return <div></div>
+              }
+            })}
+          </div>
+        })}
       </header>
     </div>
   );
